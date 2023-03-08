f6f61e5878789b210e25677ae00e42b8cd8691dc
t_borie_catie <t.borie@catie.fr>
2023-03-08 14:14:22 +0100

    Removed ST-Link probe option w/ associated README

--- sixtron_flash/cli.py
+++ sixtron_flash/cli.py
@@ -9,16 +9,9 @@ import json
 
 
 @click.command()
-@click.argument("JLINK_DEVICE", nargs=1, required=False)
-@click.argument("FILE_PATH", type=click.Path(exists=True), required=False)
-@click.option(
-    "-p",
-    "--probe",
-    default="j-link",
-    type=click.Choice(["j-link", "st-link"]),
-    show_default=True,
-)
-def main(jlink_device, file_path, probe):
+@click.argument("JLINK_DEVICE", nargs=1)
+@click.argument("FILE_PATH", type=click.Path(exists=True))
+def main(jlink_device, file_path):
     """Console tool to flash 6TRON boards."""
     click.echo("6TRON Flash Tool")
 
@@ -65,100 +58,56 @@ def main(jlink_device, file_path, probe):
 
     click.echo("Flash: {}".format(file_path))
 
-    if probe == "j-link":
+    if jlink_device.lower().startswith("stm32"):
+        addr = "0x08000000"
+    else:
+        addr = "0x0"
 
-        if jlink_device.lower().startswith("stm32"):
-            addr = "0x08000000"
-        else:
-            addr = "0x0"
+    # Create the JLink command file
+    # fmt: off
+    command_file_content = (
+        "si 1\n"
+        "speed 4000\n"
+        "r\n"
+        "h\n"
+        "loadfile \"{}\",{}\n"
+        "r\n"
+        "q\n"
+    ).format(file_path, addr)
+    # fmt: on
+    command_file = open(
+        os.path.join(script_dirname, "jlink_command_file.jlink").replace("\\", "/"),
+        "w",
+    )
+    command_file.write(command_file_content)
+    command_file.close()
 
-        # Create the JLink command file
-        # fmt: off
-        command_file_content = (
-            "r\n"
-            "h\n"
-            "loadfile \"{}\",{}\n"
-            "r\n"
-            "q\n"
-        ).format(file_path, addr)
-        # fmt: on
-        command_file = open(
-            os.path.join(script_dirname, "jlink_command_file.jlink").replace("\\", "/"),
-            "w",
-        )
-        command_file.write(command_file_content)
-        command_file.close()
+    command_path = os.path.join(script_dirname, "jlink_command_file.jlink").replace(
+        "\\", "/"
+    )
 
-        command_path = os.path.join(script_dirname, "jlink_command_file.jlink").replace(
-            "\\", "/"
-        )
-
-        # Flash target
+    # Flash target
+    if os.name == "nt":
+        executable = "JLink.exe"
+    else:
+        executable = "JLinkExe"
+    # fmt: off
+    cmd = "{} -Device {} -if JTAG -ExitOnError 1 -NoGui 1 -CommandFile \"{}\" ".format(
+        executable, jlink_device, command_path
+    )
+    # fmt: on
+    ret = os.system(cmd)
+    if ret != 0:
         if os.name == "nt":
-            executable = "JLink.exe"
-        else:
-            executable = "JLinkExe"
-        # fmt: off
-        cmd = "{} -Device {} -if SWD -Speed 4000 -ExitOnError 1 -NoGui 1 -CommandFile \"{}\" ".format(
-            executable, jlink_device, command_path
-        )
-        # fmt: on
-        ret = os.system(cmd)
-        if ret != 0:
-            if os.name == "nt":
-                click.echo(
-                    "Error when running J-Link executable. Please verify that a J-Link probe is connected, and that JLink.exe has been added to the PATH"
-                )
-            elif os.name == "posix":
-                click.echo(
-                    "Error when running J-Link executable. Please verify that a J-Link probe is connected, and that JLinkExe has been added to the PATH"
-                )
-
-        os.remove(command_path)
-
-    if probe == "st-link":
-
-        if "STM32L4" in jlink_device:
-            openocd_cli_args = ' -f interface/stlink-v2-1.cfg -c "transport select hla_swd"\
-                    -f target/stm32l4x.cfg -c "reset_config srst_only srst_nogate"'
-
-            # Flash target
-            cmd = (
-                "openocd"
-                + openocd_cli_args
-                + ' -c "program {} verify reset exit"'.format(file_path)
-            )
-            ret = os.system(cmd)
-            if ret != 0:
-                if os.name == "nt":
-                    click.echo(
-                        "Error when calling OpenOCD executable. Please verify that openocd.exe has been added to the PATH"
-                    )
-                elif os.name == "posix":
-                    click.echo(
-                        "Error when calling OpenOCD executable. Please verify that openocd has been added to the PATH"
-                    )
-
-        else:  # use the config file of the project
             click.echo(
-                "Unknown device, using openocd.cfg configuration file of the project"
+                "Error when running J-Link executable. Please verify that a J-Link probe is connected, and that JLink.exe has been added to the PATH"
+            )
+        elif os.name == "posix":
+            click.echo(
+                "Error when running J-Link executable. Please verify that a J-Link probe is connected, and that JLinkExe has been added to the PATH"
             )
 
-            # Flash target
-            cmd = 'openocd -f openocd.cfg -c "program {} verify reset exit"'.format(
-                file_path
-            )
-            ret = os.system(cmd)
-            if ret != 0:
-                if os.name == "nt":
-                    click.echo(
-                        "Error when calling OpenOCD executable. Please verify that openocd.exe has been added to the PATH"
-                    )
-                elif os.name == "posix":
-                    click.echo(
-                        "Error when calling OpenOCD executable. Please verify that openocd has been added to the PATH"
-                    )
-            click.echo()
+    os.remove(command_path)
 
     return 0
 
